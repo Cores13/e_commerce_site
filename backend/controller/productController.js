9260b3ff8012bdbce40f10f07492e259e880f91b
Cores13 <64189850+Cores13@users.noreply.github.com>
2021-05-12 14:46:19 +0200

    productRoutes and productController
    Made productRoutes and productController with getProductById and getAllProducts methods.

---
+++ backend/controller/productController.js
@@ -0,0 +1,29 @@
+const product = require('..models/product');
+const Product = require('../models/Product');
+
+const getAllProducts = async (req, res) => {
+    try{
+        const products = await Product.fint();
+
+        res.json(products);
+    }catch(err){
+        console.error(err);
+        res.status(500).json({message: 'Server error.'});
+    }
+};
+
+const getProductById = async (req, res) => {
+    try{
+        const product = await Product.fint(req.params.id);
+
+        res.json(product);
+    }catch(err){
+        console.error(err);
+        res.status(500).json({message: 'Server error.'});
+    }
+};
+
+module.exports = {
+    getAllProducts,
+    getProductById,
+}
