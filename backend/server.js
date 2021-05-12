c898cd4488cf74d8bacde982a9ed5e35e811e4d1
Cores13 <64189850+Cores13@users.noreply.github.com>
2021-05-12 13:10:26 +0200

    create-react-app

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,8 @@
+require('dotenv').config();
 const express = require('express');
 
 const app = express();
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => console.log(` ${PORT}`));
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
