79eb3e2028d63f6144307dcc2f96ee70fef42914
Cores13 <64189850+Cores13@users.noreply.github.com>
2021-05-14 16:00:17 +0200

    setting up redux

---
+++ frontend/src/redux/store.js
@@ -0,0 +1,9 @@
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
+import {composeWithDevTools} from 'redix-devtools-extension';
+
+const reducer = combineReducers({
+
+});
+
+const 
