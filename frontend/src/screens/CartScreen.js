8d7075b82fe909b7d65de8e45eb41647577ef13a
Cores13 <64189850+Cores13@users.noreply.github.com>
2021-05-14 15:56:37 +0200

    CartScreen frontend done
    Made CartScreen with CartItem components displaying

--- frontend/src/screens/CartScreen.js
+++ frontend/src/screens/CartScreen.js
@@ -1,11 +1,27 @@
 import './CartScreen.css';
 
-function CartScreen() {
-    return (
-        <div className="cartscreen">
-            Cart Screen
-        </div>
-    )
-}
+//Components
+import CartItem from '../components/CartItem';
 
-export default CartScreen
+const CartScreen = () => {
+    return <div className="cartscreen">
+            <div className="cartscreen__left">
+                <h2>Shopping Cart</h2>
+
+                <CartItem />
+                <CartItem />
+                <CartItem />
+            </div>
+            <div className="cartscreen__right">
+                <div className="cartscreen__info">
+                    <p>Subtotal: (0) items</p>
+                    <p>Total: $499.99</p>
+                </div>
+                <div>
+                    <button>Proceed To Checkout</button>
+                </div>
+            </div>
+        </div>
+};
+
+export default CartScreen;
--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -1,11 +1,44 @@
 import './ProductScreen.css';
 
-function ProductScreen() {
-    return (
-        <div className="productscreen">
-            Product Screen
+const ProductScreen = () => {
+    return <div className="productscreen">
+        <div className="productscreen__left">
+            <div className="left__image">
+                <img src="https://images.unsplash.com/photo-1606813907291-d86efa9b94db?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1352&q=80"
+                alt='product name'/>
+            </div>
+            <div className="left__info">
+                <p className="left__name">Product 1</p>
+                <p className="left__price">Price: $499.99</p>
+                <p className="left__description">Description: eopzjihprdsjthos
+                serhsr</p>
+            </div>
         </div>
-    )
+        
+        <div className="productscreen__right">
+            <div className="right__info">
+                <p>
+                    Price: <span>$499.99</span>
+                </p>
+                <p>
+                    Status: <span>In Stock</span>
+                </p>
+                <p>
+                    Quantity
+                    <select>
+                        <option value='1'>1</option>
+                        <option value='2'>2</option>
+                        <option value='3'>3</option>
+                        <option value='4'>4</option>
+                    </select>
+                </p>
+                <p>
+                    <button type="button">Add To Cart</button>
+                </p>
+            </div>
+        </div>
+            
+        </div>
 }
 
 export default ProductScreen
